24274fc7567b482143f4afd44dc8aeb99a79509e
mcdaniip <mcdaniip@mail.uc.edu>
2025-02-25 18:36:25 -0500

    Add project files.

---
+++ Beautiful_Budgies_Assignment06/mainPackage/main.py
@@ -0,0 +1,19 @@
+# File Name : main.py
+# Student Name: Ian McDaniel, Sharvari Girish Patil, Nate Hoang, Michael Slivinski
+# email:  {required}
+# Assignment Number: Assignment 06
+# Due Date:   February 25 2025
+# Course #/Section:   IS4010 - 001
+# Semester/Year:   Spring Semester 2025
+# Brief Description of the assignment:  
+
+# Brief Description of what this module does. 
+# Citations: 
+
+# Anything else that's relevant:
+
+from birdPackage.bird import *
+from foodPackage.food import *
+from cagePackage.cage import *
+
+if __name__ == "__main__":
