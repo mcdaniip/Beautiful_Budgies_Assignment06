86bc7d9c53e6743ef9c6cd788553db9630cd7b38
mcdaniip <mcdaniip@mail.uc.edu>
2025-02-26 22:13:12 -0500

    added code to main for classes bird and food

--- Beautiful_Budgies_Assignment06/mainPackage/main.py
+++ Beautiful_Budgies_Assignment06/mainPackage/main.py
@@ -17,3 +17,23 @@ from foodPackage.food import *
 from cagePackage.cage import *
 
 if __name__ == "__main__":
+    budgie = Bird("Budgie")
+    print(type(budgie))
+    print(budgie)
+    budgie.flaps()
+
+    print("")
+
+    feeder = BirdFeeder(500, "Backyard Shepherd Hook")
+    print(type(feeder))
+    print(feeder)
+    feeder.bird_eats(100)
+    print(f"Current food: {feeder.get_food_grams} grams")
+    print(f"Feeder location: {feeder.get_location}")
+    feeder.squirrel_eats(300)
+    print(feeder)
+    feeder.refill_food(400)
+    print(feeder)
+
+
+    print("")
